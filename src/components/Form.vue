b480af9783d18b23164e0cf2921b38e3f02b04a2
younesmjl <younes.manjal@gmail.com>
2021-02-16 21:46:49 +0100

    bind variable form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,10 +1,73 @@
 <template>
   <h3>Créer une tâche</h3>
+  <form>
+    <input type="text" placeholder="Nom de latâche" v-model="name" />
+    <br />
+    <textarea
+      cols="30"
+      rows="10"
+      v-model="description"
+      placeholder="Description de la tâche"
+    >
+    </textarea>
+    <br />
+    <select v-model="temporality">
+      <option
+        v-for="tempo in temporalityTypes"
+        v-bind:value="tempo.value"
+        v-bind:key="tempo.id"
+      >
+        {{ tempo.name }}
+      </option>
+    </select>
+    <br />
+    <button>créer</button>
+  </form>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+
+export default {
+  name: "Form",
+  setup() {
+    const name = ref("");
+    const description = ref("");
+    const temporalityTypes = ref([
+      {
+        id: 1,
+        name: "court terme",
+        value: "short-term",
+      },
+      {
+        id: 2,
+        name: "Moyen terme",
+        value: "medium-term",
+      },
+      {
+        id: 3,
+        name: "long terme",
+        value: "long-term",
+      },
+    ]);
+    const temporality = ref("short-term");
+
+    return {
+      name,
+      description,
+      temporalityTypes,
+      temporality,
+    };
+  },
+};
 </script>
 
-<style>
+<style scoped>
+input,
+textarea,
+select,
+button {
+  width: 90%;
+  margin: 5px 10px;
+}
 </style>
