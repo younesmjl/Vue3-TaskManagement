60044605e2b21c4f5a58566674fe00aedf1fa1e0
younesmjl <younes.manjal@gmail.com>
2021-02-16 21:05:06 +0100

    ajout composant vue

---
+++ src/components/Form.vue
@@ -0,0 +1,10 @@
+<template>
+  <h3>Créer une tâche</h3>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
     <HelloWorld msg="Progressr (compoAPI)" />
+   <Form />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import Form from "@/components/Form.vue"
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Form,
   }
 };
 </script>
