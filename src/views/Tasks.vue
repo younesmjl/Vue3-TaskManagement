cb554c96b54f6a168ea2e5afed95879059da61ef
younesmjl <younes.manjal@gmail.com>
2021-08-11 13:17:18 +0200

    recuperation saisie utlisateurs

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,4 +1,11 @@
 <template>
+  <input
+    class="border-all-primary border-width-1"
+    type="text"
+    placeholder="Filtrer"
+    v-model="letters"
+    @keyup="filter"
+  />
   <div v-for="task in tasks" :key="task.id">
     <div class="border-all-secondary with-margin-all-20">
       <h3>Titre : {{ task.name }}</h3>
@@ -15,8 +22,14 @@ export default {
   name: "Tasks",
   setup() {
     const tasks = ref("");
+    const letters = ref("");
     tasks.value = tasksService.read();
-    return { tasks, tasksService };
+
+    function filter() {
+      console.log(letters.value);
+    }
+
+    return { tasks, tasksService, letters, filter };
   },
 };
 </script>
